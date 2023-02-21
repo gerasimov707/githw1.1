274b58b802fa39a6127fbfeaa6080545540b2ec7
Gerasimov Alexandr <gerasimov707@yandex.ru>
2023-02-21 17:10:19 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,54 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String[] products = new String[]{"Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка"};
+    public static int[] prices = new int[]{50, 135, 65, 53};
+    public static int MIN_COST_FOR_BONUS = 1000;
 
-    public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65, 53 };
-
-    // В стоимости этих товаров каждые три товара должны стоить как два:
-    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+    public Main() {
+    }
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
         System.out.println("Наш ассортимент:");
-        for (int i = 0; i < products.length; i++) {
+
+        for(int i = 0; i < products.length; ++i) {
             System.out.println("\t" + (i + 1) + ". " + products[i] + " за " + prices[i] + " за шт. ");
         }
+
         System.out.println();
-
         Scanner scanner = new Scanner(System.in);
-
         int[] counts = new int[products.length];
 
-        while (true) {
+        while(true) {
             System.out.print("Введите номер товара и количество через пробел или end: ");
             String line = scanner.nextLine();
-
+            int i;
             if ("end".equals(line)) {
-                break;
-            }
+                System.out.println("Ваша корзина покупок:");
+                int sum = 0;
 
-            String[] parts = line.split(" ");
-            int productNum = Integer.parseInt(parts[0]) - 1;
-            int productCount = Integer.parseInt(parts[1]);
+                for(i = 0; i < products.length; ++i) {
+                    sum += prices[i] * counts[i];
+                }
 
-            counts[productNum] += productCount;
-        }
+                boolean doBonus = sum >= MIN_COST_FOR_BONUS;
 
-        System.out.println("Ваша корзина покупок:");
-        int sum = 0;
-        for (int i = 0; i < products.length; i++) {
-            if (counts[i] != 0) {
-                boolean isOnSale = false;
-                for (String saleProduct : productsOnSale) {
-                    if (products[i].equals(saleProduct)) {
-                        isOnSale = true;
+                for(i = 0; i < products.length; ++i) {
+                    if (counts[i] != 0) {
+                        String var10001 = products[i];
+                        System.out.println("\t" + var10001 + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + prices[i] * counts[i] + " руб.");
                     }
                 }
 
-                if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
-                    sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
-                } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
-                    sum += prices[i] * counts[i];
-                }
+                System.out.println("Итого: " + sum + " руб.");
+                return;
             }
-        }
-        System.out.println("Итого: " + sum + " руб.");
-    }
 
+            String[] parts = line.split(" ");
+            i = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+            counts[i] += productCount;
+        }
+    }
 }
